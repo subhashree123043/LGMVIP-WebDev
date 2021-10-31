8db7d7a56cba73068e33e153e20e21fe029923c9
subhashree123043 <85428576+subhashree123043@users.noreply.github.com>
2021-10-31 16:49:31 +0530

    initial

--- task3lgm/SRS/STUDENT_RESULT.sql
+++ task3lgm/SRS/STUDENT_RESULT.sql
@@ -1,195 +1,201 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 25, 2021 at 03:54 PM
--- Server version: 8.0.26-0ubuntu0.20.04.3
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2021 at 12:00 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!40101 SET NAMES utf8 */
-;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `STUDENT_RESULT`
+-- Database: `task3`
 --
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `admin`
 --
+
 CREATE TABLE `admin` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `pass` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Dumping data for table `admin`
 --
-INSERT INTO `admin` (`id`, `name`, `pass`, `email`)
-VALUES (1, 'subhashree', '12345', 'admin123@gmail.com');
+
+INSERT INTO `admin` (`id`, `name`, `pass`, `email`) VALUES
+(1, 'subhashree', '12345', 'subhashree123@gmail.com');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `branch`
 --
+
 CREATE TABLE `branch` (
-  `id` int NOT NULL,
-  `class` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+  `id` int(11) NOT NULL,
+  `class` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Dumping data for table `branch`
 --
-INSERT INTO `branch` (`id`, `class`)
-VALUES (1, 'mca'),
-  (3, 'bca'),
-  (4, 'mba'),
-  (6, 'B.SC');
+
+INSERT INTO `branch` (`id`, `class`) VALUES
+(1, 'mca'),
+(3, 'bca'),
+(4, 'mba');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `result`
 --
+
 CREATE TABLE `result` (
-  `res_id` int NOT NULL,
+  `res_id` int(11) NOT NULL,
   `res_reg` varchar(100) NOT NULL,
   `res_branch` varchar(100) NOT NULL,
-  `os` int NOT NULL,
-  `co` int NOT NULL,
-  `java` int NOT NULL,
-  `dbms` int NOT NULL,
-  `ds` int NOT NULL,
-  `ai` int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+  `os` int(11) NOT NULL,
+  `co` int(11) NOT NULL,
+  `java` int(11) NOT NULL,
+  `dbms` int(11) NOT NULL,
+  `ds` int(11) NOT NULL,
+  `ai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Dumping data for table `result`
 --
-INSERT INTO `result` (
-    `res_id`,
-    `res_reg`,
-    `res_branch`,
-    `os`,
-    `co`,
-    `java`,
-    `dbms`,
-    `ds`,
-    `ai`
-  )
-VALUES (1, '2005106019', '1', 69, 61, 68, 62, 69, 65);
+
+INSERT INTO `result` (`res_id`, `res_reg`, `res_branch`, `os`, `co`, `java`, `dbms`, `ds`, `ai`) VALUES
+(1, '2005106019', '1', 69, 61, 68, 62, 69, 65),
+(3, '200510603', '1', 76, 60, 87, 78, 79, 89),
+(4, '2005016023', '1', 65, 78, 67, 78, 78, 87),
+(5, '2005106010', '1', 48, 65, 63, 56, 53, 65),
+(6, '2005106011', '1', 34, 45, 48, 54, 67, 56),
+(7, '200510', '1', 56, 67, 69, 45, 43, 56),
+(8, '2005106', '1', 54, 56, 65, 67, 54, 59),
+(9, '20051060', '1', 46, 67, 55, 54, 68, 69);
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `student`
 --
+
 CREATE TABLE `student` (
-  `id` int NOT NULL,
+  `sid` int(11) NOT NULL,
   `st_name` varchar(100) NOT NULL,
   `st_reg` varchar(100) NOT NULL,
   `st_email` varchar(100) NOT NULL,
   `st_branch` varchar(30) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Dumping data for table `student`
 --
-INSERT INTO `student` (
-    `id`,
-    `st_name`,
-    `st_reg`,
-    `st_email`,
-    `st_branch`
-  )
-VALUES (
-    2,
-    'Subhashree Panda',
-    '2005106011',
-    'subha123@gmail.com',
-    '1'
-  ),
-  (
-    3,
-    'Elie panda',
-    '2005106019',
-    'elie123@gmail.com',
-    '1'
-  );
+
+INSERT INTO `student` (`sid`, `st_name`, `st_reg`, `st_email`, `st_branch`) VALUES
+(11, 'Silu Mishra', '2005106', 'mishra123@gmail.com', '1'),
+(13, 'Mukul Panda', '20051060', 'panda123@gmail.com', '1');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `year`
 --
+
 CREATE TABLE `year` (
-  `id` int NOT NULL,
-  `bid` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `bid` int(11) NOT NULL,
   `year` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
+
 --
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `branch`
 --
 ALTER TABLE `branch`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `result`
 --
 ALTER TABLE `result`
-ADD PRIMARY KEY (`res_id`);
+  ADD PRIMARY KEY (`res_id`);
+
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`sid`);
+
 --
 -- Indexes for table `year`
 --
 ALTER TABLE `year`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
 --
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-MODIFY `id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-MODIFY `res_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
+  MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-MODIFY `id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 4;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `year`
 --
 ALTER TABLE `year`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
